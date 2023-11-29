bf9bf86da86de25744fd541cd89501fe7f588ebb
dolphinium <dolphofficial@gmail.com>
2023-11-29 10:46:50 +0300

    end of first part.

---
+++ android/app/src/main/kotlin/com/dolphinium/stopwatch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dolphinium.stopwatch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/stopwatch.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class StopWatch extends StatefulWidget{
+  @override
+  State createState() => StopWatchState();
+}
+
+
+class StopWatchState extends State<StopWatch>{
+  late int seconds;
+  late Timer timer;
+
+  @override
+  void initState(){
+    super.initState();
+
+    seconds = 0;
+    timer = Timer.periodic(Duration(seconds: 1), _onTick);
+  }
+  void _onTick(Timer time){
+    setState(() {
+      ++seconds;
+    });
+  }
+
+  String _secondsText() => seconds == 1 ? 'second':"seconds";
+
+  @override
+  void dispose(){
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stopwatch"),
+      ),
+      body: Center(
+        child: Text(
+          '$seconds ${_secondsText()}',
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
+    );
+  }
+}
